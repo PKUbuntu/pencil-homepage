7606ae9d98903d002fd68e4a925e052a5964db5b
PKUbuntu <xiaolang001@gmail.com>
2026-03-19 10:28:16 +0800

    fix: use HashRouter for GitHub Pages compatibility
    - Replace BrowserRouter with HashRouter for reliable SPA routing
    - Remove 404.html redirect hack (not needed with hash routing)
    - Simplify index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
